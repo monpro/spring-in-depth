ba397935721a3b8e85a63bc408d99885643a6472
monpro <monproshen@gmail.com>
2021-05-02 14:02:10 +1000

    The proxy mode allows Spring container to inject a new object into the singleton bean
    The prototype bean is not autowired into the singleton bean at the time of its creation. Instead a proxy(like a placeholder) is autowired. It adds a level of indirection. When requesting the prototype bean from spring, a new instance of prototype bean is created and returned by the application context.

--- src/main/java/com/monpro/springindepth/springsystem/Movie.java
+++ src/main/java/com/monpro/springindepth/springsystem/Movie.java
@@ -2,10 +2,11 @@ package com.monpro.springindepth.springsystem;
 
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Component;
 
 @Component
-@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+@Scope(value = ConfigurableBeanFactory.SCOPE_PROTOTYPE, proxyMode = ScopedProxyMode.TARGET_CLASS)
 public class Movie {
   private static int instances = 0;
 
@@ -18,5 +19,4 @@ public class Movie {
   public static int getInstances() {
     return Movie.instances;
   }
-
 }
